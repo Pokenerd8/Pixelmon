a3df0d36604a940c15bacfdd659d8f74ac7c79d1
Yaseen <Yaseen98@hotmail.co.uk>
2012-12-27 01:50:22 +0000

    Added Ornithologist

--- minecraft/pixelmon/enums/EnumTrainers.java
+++ minecraft/pixelmon/enums/EnumTrainers.java
@@ -5,6 +5,7 @@ public enum EnumTrainers {
 	Fisherman,
 	//FemaleRocketGrunt, This model needs to be pieced correctly.
 	BugCatcher;
+	Ornithologist;
 
 	public static boolean has(String creatureName) {
 		for (EnumTrainers trainer : values())
